8f52ef58a398b5501e1c3a111c198d30874274d1
Piotr Lewiński <lewinskip@EMEA.LOCAL>
2018-01-26 17:54:07 +0100

    js menu dev

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
 $(function () {
     //Scroll to 
-    $('a[href^="#"]').on('click', function (event) {
+    /*$('a[href^="#"]').on('click', function (event) {
         var target = $(this.getAttribute('href'));
-
         if (target.length) {
             event.preventDefault();
             $('html, body').stop().animate({
                 scrollTop: target.offset().top
             }, 1000);
         }
+    });*/
+    var flag = true;   
+    $('.menu-mobile-item').on("click", function() {
+        console.log("ckjucj");
+        flag = !flag;
+        if (!flag) {    
+        $('nav#main-nav').addClass("mobile-menu");
+            console.log("piewrszy if");
+    }
+        else {
+            $('nav#main-nav').removeClass("mobile-menu");
+            console.log("drugi if");
+        }
+        
     });
-    
 });
