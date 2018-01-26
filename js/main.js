426f092a9f51cb6496a5c8c65264e6b94eccf64c
Piotr Lewiński <lewinskip@EMEA.LOCAL>
2018-01-26 20:32:34 +0100

    pict description js dev

--- js/main.js
+++ js/main.js
@@ -23,6 +23,79 @@ $(function () {
         });
     }
     //Main-nav - Stop*/
+    
+    /* Section ABOUT - Start (PICTURE) */
+    $('.behind-img').mouseenter(
+        function () {
+            $('.html').text('90');
+            $('.css').html('80');
+            $('.js').html('67');
+            $('.jquery').html('65');
+            $('.skills').addClass('bar-animation');
+            $('.bar-animation').each(function () {
+                $(this).prop('Counter', 0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
+
+            });
+        }
+    );
+    $('.behind-img').mouseleave(
+        function () {
+            $('.skills').removeClass('bar-animation');
+            setTimeout(function () {
+                $('.skills').html('0');
+            }, 200);
+        });
+    /* Section ABOUT - Stop (PICTURE) */
+    
+     /* Section About - PICTURE DESCRIPTION - Start*/
+    var descOneText = $('.programmer-description-item[0]').text();
+    var descOne = $('.programmer-description-item[0]');
+    var descTwoText = $('.programmer-description-item[1]').text();
+    var descTwo = $('.programmer-description-item[1]');
+    var descThreeText = $('.programmer-description-item[2]').text();
+    var descThree = $('.programmer-description-item[2]');
+    
+    $('.programmer-description-item').each(function(index) {
+        $(this).text('');
+    });
+
+    
+
+    var a = 1;
+    var b = 1;
+    var c = 1;
+
+    /*function typeWriter() {
+
+        if (a < rowOne.length) {
+            rowOneZ.innerHTML += rowOne.charAt(a);
+            a++;
+            setTimeout(typeWriter, 50);
+        }
+
+        if (a >= rowOne.length && b < rowTwo.length) {
+            rowTwoZ.innerHTML += rowTwo.charAt(b);
+            b++;
+            setTimeout(typeWriter, 100);
+        }
+
+        if (b >= rowTwo.length && c < rowThree.length) {
+            rowThreeZ.innerHTML += rowThree.charAt(c);
+            c++;
+            setTimeout(typeWriter, 150);
+        }
+    }
+
+    typeWriter();*/
+    /* Section ABOUT - Stop */
 
     //Animate scroll  - Start
     $('a[href^="#"]').on('click', function (event) {
