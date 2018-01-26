fea8e6abfe2e93bf1fc156f05f39b8262a210815
Piotr Lewiński <lewinskip@EMEA.LOCAL>
2018-01-26 19:49:26 +0100

    bar-skills update

--- js/main.js
+++ js/main.js
@@ -1,35 +1,39 @@
 $(function () {
-    //Scroll to 
-    /*$('a[href^="#"]').on('click', function (event) {
+
+    //Main-nav - Start
+    var windowWidth = $(window).width();
+    if (windowWidth < 680) {
+        var flag = true;
+
+        function mobileMenu() {
+            flag = !flag;
+            if (!flag) {
+                $('nav#main-nav').addClass("mobile-menu");
+            } else {
+                $('nav#main-nav').removeClass("mobile-menu");
+            }
+        };
+
+        $('.menu-mobile-item').on("click", function () {
+            mobileMenu();
+        });
+
+        $('.list-menu-item').on("click", function () {
+            mobileMenu();
+        });
+    }
+    //Main-nav - Stop*/
+
+    //Animate scroll  - Start
+    $('a[href^="#"]').on('click', function (event) {
         var target = $(this.getAttribute('href'));
+
         if (target.length) {
             event.preventDefault();
             $('html, body').stop().animate({
                 scrollTop: target.offset().top
-            }, 1000);
+            }, 1200);
         }
-    });*/
-    
-    /*main-nav section - start*/
-    
-    var flag = true;
-    function mobileMenu() {        
-        flag = !flag;
-        if (!flag) {
-            $('nav#main-nav').addClass("mobile-menu");            
-        } else {
-            $('nav#main-nav').removeClass("mobile-menu");            
-        }
-    };
-
-    $('.menu-mobile-item').on("click", function(){
-      mobileMenu();  
     });
-    
-    $('.list-menu-item').on("click", function(){
-      mobileMenu();  
-    });
-    
-    /*main-nav section - stop*/
-    
+    //Animate scroll  - Stop
 });
